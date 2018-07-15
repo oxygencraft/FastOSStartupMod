89586702b4355ca94bbbf33910fccaa22e3f09a2
oxygencraft <21054297+oxygencraft@users.noreply.github.com>
2018-07-15 19:13:51 +1000

    This is literally a very simple mod lol

---
+++ FastOSStartupMod/FastOSStartupMod/FastOSStartupMod.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pathfinder.ModManager;
+
+namespace FastOSStartupMod
+{
+    public class FastOSStartupMod : IMod
+    {
+        public void Load()
+        {
+            Console.WriteLine("Fast OS Startup Enabled");
+        }
+
+        public void LoadContent()
+        {
+            Hacknet.Settings.slowOSStartup = false;
+            Hacknet.Settings.osStartsWithTutorial = false;
+        }
+
+        public void Unload()
+        {
+            Console.WriteLine("Fast OS Startup Disabled");
+        }
+
+        public string Identifier{ get { return "FastOSStartupMod"; } }
+    }
+}
